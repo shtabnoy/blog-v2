41bf92897d1b44bc9e7f1f75e1897f3b0fe0663b
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2020-04-20 23:27:01 +0200

    show article cover

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
+import React from 'react'
 import { gql, useQuery } from '@apollo/client'
 
 const GET_ARTICLES = gql`
@@ -11,19 +12,31 @@ const GET_ARTICLES = gql`
       user {
         username
       }
+      cover {
+        url
+        name
+      }
       created_at
       updated_at
     }
   }
 `
 
-const HomePage = (): JSX.Element => {
+const HomePage: React.FC = () => {
   const { loading, error, data } = useQuery(GET_ARTICLES)
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
 
-  return data.articles.map((article: any) => <div>{article.title}</div>)
+  return data.articles.map((article: any) => (
+    <React.Fragment>
+      <img
+        src={'http://localhost:1337' + article.cover.url}
+        alt={article.cover.name}
+      />
+      <div key={article.id}>{article.title}</div>
+    </React.Fragment>
+  ))
 }
 
 export default HomePage
