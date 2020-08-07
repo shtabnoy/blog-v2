028cd067e3fc358d39958a494990bc009b73b20d
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2020-08-07 23:50:00 +0200

    feat: markdown body and styles

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,29 +1,61 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
-import React from "react";
-import { useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-import { GET_ARTICLE } from "../queries";
+import { jsx, css } from '@emotion/core'
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import { GET_ARTICLE } from '../queries'
+import theme from '../utils/colors'
+import ReactMarkdown from 'react-markdown'
+import styled from '@emotion/styled'
 
 interface ArticleProps {}
 
+const pageStyles = css`
+  background: linear-gradient(
+    to bottom,
+    ${theme.main.bgPrimary},
+    ${theme.main.bgSecondary}
+  );
+  background-attachment: fixed;
+  color: white;
+
+  p {
+    margin: 36px 0;
+  }
+  h1 {
+    margin: 0 0 72px;
+  }
+  h2,
+  h3 {
+    text-indent: 32px;
+  }
+`
+
+const Content = styled.main`
+  margin: 0 auto;
+  padding: 100px 0;
+  max-width: 680px;
+`
+
 const Article: React.FC<ArticleProps> = () => {
-  const params = useParams<{ id: string }>();
+  const params = useParams<{ id: string }>()
   const { data, loading, error } = useQuery(GET_ARTICLE, {
     variables: {
       id: params.id,
     },
-  });
+  })
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
 
   return (
-    <React.Fragment>
-      <div>{data.article.title}</div>
-      <div>{data.article.body}</div>
-    </React.Fragment>
-  );
-};
+    <div css={pageStyles}>
+      <Content>
+        <h1>{data.article.title}</h1>
+        <ReactMarkdown source={data.article.body} />
+      </Content>
+    </div>
+  )
+}
 
-export default Article;
+export default Article
